3537ba5ce12f78442626e9b080c96221e322146a
Emmanuel <emmanuelcu@gmail.com>
2015-10-31 18:05:49 -0600

    Initial Commit

---
+++ MountGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MountGame
+//
+//  Created by Emmanuel Cuevas on 10/31/15.
+//  Copyright © 2015 Emmanuel Cuevas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MountGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MountGame
+//
+//  Created by Emmanuel Cuevas on 10/31/15.
+//  Copyright © 2015 Emmanuel Cuevas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
